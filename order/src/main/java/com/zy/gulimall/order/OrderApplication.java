587d5d1c01b0abfbf9fcac1e7c5f3467643b68a9
zy-zhangyi <994287042@qq.com>
2021-12-25 22:01:19 +0800

    update

---
+++ order/src/main/java/com/zy/gulimall/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.zy.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
